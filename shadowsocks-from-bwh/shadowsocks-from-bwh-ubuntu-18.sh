88f9fdf21a00df178da83326e2270923f6d1d390
Johnny Gu <johnny@wiredcraft.com>
2019-02-26 23:37:44 +0800

    init

---
+++ shadowsocks-from-bwh/shadowsocks-from-bwh-ubuntu-18.sh
@@ -0,0 +1,23 @@
+sudo apt update
+sudo apt install python2.7 python-pip
+
+pip install supervisor
+pip install shadowsocks
+
+cp ./configs/* ~/
+
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar zxf LATEST.tar.gz
+cd libsodium*
+./configure
+sudo make && sudo make install
+
+cat <<EOT >> /etc/ld.so.conf
+/lib
+/usr/lib64
+/usr/local/lib
+EOT
+
+sudo ldconfig
+
+sudo ssserver -c ~/shadowsocks.json start
