296cc5d58f2c24183f8a09c2f635e99434dafbed
Johnny Gu <johnny@wiredcraft.com>
2019-06-17 16:29:42 +0800

    add v2ray setup bash

---
+++ v2ray-setup/v2ray-ubuntu-18.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install python2.7 python-pip
+sudo apt install curl
+
+pip install supervisor
+
+cp ./configs/* ~/
+cp /etc/v2ray/config.json ~/v2ray.json
+
+bash <(curl -L -s https://install.direct/go.sh)
+
+print "config reference https://intmainreturn0.com/v2ray-config-gen/"
+
+/usr/bin/v2ray/v2ray -config ~/v2ray.json
